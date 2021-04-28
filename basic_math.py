a6f993651d680193320b3cd8a01b0ca03471cd3a
Nika Zamani <nika.zamani@mths.ca>
2021-04-28 04:14:11 +0000

    added area_and_perimeter.py

---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by: Nika Zamani
+# Created on: April 2021
+# This program does basic math
+
+
+def main():
+    # this function does basic math
+    print(9 + 2)
+    print("7 - 3={}" .format(7 - 3))
+    print("4 / 2={}" .format(4 / 2))
+    print("4 + 4 / 2={}" .format(4 + 4 / 2))
+    print("3 + 4 * 2 ={}" .format(3 + 4 * 2))
+    print("5 + 2 ^ 3={}" .format(5 + 2 ^ 3))  # bitwise operand
+    print("5 + 2**3={}" .format(5 + 2**3))  # exponent
+
+
+if __name__ == "__main__":
+    main()
